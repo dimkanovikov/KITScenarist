34f66bb760be131924b89b7c9b3ae163da67d965
Dimka Novikov <to@dimkanovikov.pro>
2014-01-17 07:11:46 +0400

    Use Fusion style for application

--- src/Scenarius.pro
+++ src/Scenarius.pro
@@ -4,9 +4,7 @@
 #
 #-------------------------------------------------
 
-QT       += core gui sql xml
-
-greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+QT       += core gui sql xml widgets
 
 TARGET = Scenarius
 TEMPLATE = app
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
-
+#include <QStyleFactory>
+#include <QStyle>
 #include <QFontDatabase>
 
 #include <UserInterface/widget.h>
@@ -8,7 +9,8 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	a.setStyle("Cleanlooks");
+	QApplication::setStyle(QStyleFactory::create("Fusion"));
+	QApplication::setPalette(QApplication::style()->standardPalette());
 
 	//
 	// Загрузим шрифт Courier New в базу шрифтов программы, если его там ещё нет
