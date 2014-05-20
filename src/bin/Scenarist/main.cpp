8276ba5559990449463748181a79fc213134f776
Dimka Novikov <to@dimkanovikov.pro>
2014-05-20 09:46:04 +0400

    Release 0.2.5

--- src/bin/Scenarist/main.cpp
+++ src/bin/Scenarist/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	application.setOrganizationName("DimkaNovikov labs.");
 	application.setOrganizationDomain("dimkanovikov.pro");
 	application.setApplicationName("Scenarist");
-    application.setApplicationVersion("0.2.4");
+    application.setApplicationVersion("0.2.5");
 
 	//
 	// Настроим стиль отображения внешнего вида приложения
