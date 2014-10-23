b27f98e82bb5ff98fd4353962ae1f1bd8f0220e0
Dimka Novikov <to@dimkanovikov.pro>
2014-10-23 12:54:12 +0400

    Fix Mac OS buindle build appearence

--- build/Mac/create_dmg.sh
+++ build/Mac/create_dmg.sh
@@ -7,16 +7,16 @@ rm ./Scenarist.dmg
 ~/Qt/5.3/clang_64/bin/macdeployqt ../Release/bin/Scenarist/Scenarist.app
 
 # скопировать в него ханспел
-cp ../Release/libs/hunspell/libhunspell.1.0.0.dylib ../Release/bin?Scenarist/Scenarist.app/Contents/Frameworks/
-cp ../Release/libs/hunspell/libhunspell.1.0.dylib ../Release/bin?Scenarist/Scenarist.app/Contents/Frameworks/
-cp ../Release/libs/hunspell/libhunspell.1.dylib ../Release/bin?Scenarist/Scenarist.app/Contents/Frameworks/
-cp ../Release/libs/hunspell/libhunspell.dylib ../Release/bin?Scenarist/Scenarist.app/Contents/Frameworks/
+cp ../Release/libs/hunspell/libhunspell.1.0.0.dylib ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/
+cp ../Release/libs/hunspell/libhunspell.1.0.dylib ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/
+cp ../Release/libs/hunspell/libhunspell.1.dylib ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/
+cp ../Release/libs/hunspell/libhunspell.dylib ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/
 
 # скопируем app-файл в текущую папку
 cp -R ../Release/bin/Scenarist/Scenarist.app ./Scenarist.app
 
 # запустить скрипт создания dmg-файла
-./make_dmg.sh -i ../../src/bin/Scenarist/logo.icns -b cover.png -c "460:251:174:256" -s "640:400"  Scenarist.app
+./make_dmg.sh -i ../../src/bin/Scenarist/logo.icns -b cover.png -c "462:252:176:258" -s "640:400"  Scenarist.app
 # -c "167:226:425:226" -s "540:400" 
 
 # удалить app-файл
