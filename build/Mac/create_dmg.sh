45743a0147b68c3d4453e77569026ca1190e1f9f
Dimka Novikov <to@dimkanovikov.pro>
2014-12-22 19:22:42 +0300

    Fix Mac build script

--- build/Mac/create_dmg.sh
+++ build/Mac/create_dmg.sh
@@ -8,7 +8,7 @@ rm ./Scenarist.dmg
 #
 # сформировать app-файл
 #
-~/Qt/5.3/clang_64/bin/macdeployqt ../Release/bin/Scenarist/Scenarist.app
+~/Qt/5.4/clang_64/bin/macdeployqt ../Release/bin/Scenarist/Scenarist.app
 
 #
 # скопировать в него библиотеки
@@ -26,8 +26,8 @@ cp -H ../Release/libs/fileformats/libfileformats.1.dylib ../Release/bin/Scenaris
 #
 # для fileformats
 #
-install_name_tool -change /Users/macuser/Qt/5.3/clang_64/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/libfileformats.1.dylib 
-install_name_tool -change /Users/macuser/Qt/5.3/clang_64/lib/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/libfileformats.1.dylib 
+install_name_tool -change /Users/macuser/Qt/5.4/clang_64/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/libfileformats.1.dylib 
+install_name_tool -change /Users/macuser/Qt/5.4/clang_64/lib/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/libfileformats.1.dylib 
 
 #
 # скопируем app-файл в текущую папку
