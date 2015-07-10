36289fd731875ed3b218f644ecc653e71285e114
Dimka Novikov <to@dimkanovikov.pro>
2015-07-10 10:18:55 +0300

    Release 0.5.5

--- src/bin/Scenarist/Application.cpp
+++ src/bin/Scenarist/Application.cpp
@@ -19,7 +19,7 @@ Application::Application(int& _argc, char** _argv) :
 	setOrganizationName("DimkaNovikov labs.");
 	setOrganizationDomain("dimkanovikov.pro");
 	setApplicationName("Scenarist");
-	setApplicationVersion("0.5.4");
+	setApplicationVersion("0.5.5");
 
 	//
 	// Настроим стиль отображения внешнего вида приложения
