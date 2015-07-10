b5c370d809bf378c24129fb1ca111923813ac6dc
Dimka Novikov <to@dimkanovikov.pro>
2015-07-10 11:29:38 +0300

    Update mac create installer script

--- build/Mac/create_dmg.sh
+++ build/Mac/create_dmg.sh
@@ -8,7 +8,7 @@ rm ./Scenarist.dmg
 #
 # сформировать app-файл
 #
-~/Qt/5.4/clang_64/bin/macdeployqt ../Release/bin/Scenarist/Scenarist.app
+~/Qt/5.5/clang_64/bin/macdeployqt ../Release/bin/Scenarist/Scenarist.app
 
 #
 # скопировать в него библиотеки
@@ -30,14 +30,14 @@ cp -H ../Release/libs/webloader/libwebloader.1.dylib ../Release/bin/Scenarist/Sc
 #
 # для fileformats
 #
-install_name_tool -change /Users/macuser/Qt/5.4/clang_64/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/libfileformats.1.dylib 
-install_name_tool -change /Users/macuser/Qt/5.4/clang_64/lib/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/libfileformats.1.dylib 
+install_name_tool -change /Users/macuser/Qt/5.5/clang_64/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/libfileformats.1.dylib 
+install_name_tool -change /Users/macuser/Qt/5.5/clang_64/lib/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/libfileformats.1.dylib 
 #
 # для webloader
 #
-install_name_tool -change /Users/macuser/Qt/5.4/clang_64/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/libwebloader.1.dylib 
-install_name_tool -change /Users/macuser/Qt/5.4/clang_64/lib/QtNetwork.framework/Versions/5/QtNetwork @executable_path/../Frameworks/QtNetwork.framework/Versions/5/QtNetwork ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/libwebloader.1.dylib 
-install_name_tool -change /Users/macuser/Qt/5.4/clang_64/lib/QtXml.framework/Versions/5/QtXml @executable_path/../Frameworks/QtXml.framework/Versions/5/QtXml ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/libwebloader.1.dylib 
+install_name_tool -change /Users/macuser/Qt/5.5/clang_64/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/libwebloader.1.dylib 
+install_name_tool -change /Users/macuser/Qt/5.5/clang_64/lib/QtNetwork.framework/Versions/5/QtNetwork @executable_path/../Frameworks/QtNetwork.framework/Versions/5/QtNetwork ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/libwebloader.1.dylib 
+install_name_tool -change /Users/macuser/Qt/5.5/clang_64/lib/QtXml.framework/Versions/5/QtXml @executable_path/../Frameworks/QtXml.framework/Versions/5/QtXml ../Release/bin/Scenarist/Scenarist.app/Contents/Frameworks/libwebloader.1.dylib 
 
 #
 # Скопируем в него системные файлы
