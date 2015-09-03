788f5786f2c2c8d7d6139ac0a3126d2506b81992
Dimka Novikov <to@dimkanovikov.pro>
2015-09-03 07:17:43 +0300

    Update Ubuntu buid intstaller script
    create installers for Russian, English and Spanish languages

--- build/Ubuntu/make-deb_amd64.sh
+++ build/Ubuntu/make-deb_amd64.sh
@@ -5,4 +5,31 @@
 # Add version update availability
 # Rename result file to choosing version and architecture
 #
+
+#
+# $1 - application version
+#
+
+#
+# Make Russian installer
+#
+echo Build Russian version
+cp -f translations/amd64/ru/control scenarist_amd64/DEBIAN/
 fakeroot dpkg-deb --build scenarist_amd64
+mv -f scenarist_amd64.deb scenarist-setup-$1_amd64.deb
+
+#
+# Make English installer
+#
+echo Build English version
+cp -f translations/amd64/en/control scenarist_amd64/DEBIAN/
+fakeroot dpkg-deb --build scenarist_amd64
+mv -f scenarist_amd64.deb scenarist-setup-$1_en_amd64.deb
+
+#
+# Make Spanish installer
+#
+echo Build English version
+cp -f translations/amd64/es/control scenarist_amd64/DEBIAN/
+fakeroot dpkg-deb --build scenarist_i386
+mv -f scenarist_amd64.deb scenarist-setup-$1_es_amd64.deb
--- build/Ubuntu/make-deb_i386.sh
+++ build/Ubuntu/make-deb_i386.sh
@@ -1,8 +1,35 @@
 #
 # TODO: 
-# Add keys for architecture i386 or amd64
+# Add keys for architecture i386 or i386
 # Add permissions check for control file: need 755
 # Add version update availability
 # Rename result file to choosing version and architecture
 #
+
+#
+# $1 - application version
+#
+
+#
+# Make Russian installer
+#
+echo Build Russian version
+cp -f translations/i386/ru/control scenarist_i386/DEBIAN/
 fakeroot dpkg-deb --build scenarist_i386
+mv -f scenarist_i386.deb scenarist-setup-$1_i386.deb
+
+#
+# Make English installer
+#
+echo Build English version
+cp -f translations/i386/en/control scenarist_i386/DEBIAN/
+fakeroot dpkg-deb --build scenarist_i386
+mv -f scenarist_i386.deb scenarist-setup-$1_en_i386.deb
+
+#
+# Make Spanish installer
+#
+echo Build English version
+cp -f translations/i386/es/control scenarist_i386/DEBIAN/
+fakeroot dpkg-deb --build scenarist_i386
+mv -f scenarist_i386.deb scenarist-setup-$1_es_i386.deb
