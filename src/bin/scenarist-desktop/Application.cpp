829f1221746faf501a581408372ac5bdc748dd1d
Dimka Novikov <to@dimkanovikov.pro>
2016-06-24 23:16:50 +0300

    Release 0.6.0 beta 3

--- src/bin/scenarist-desktop/Application.cpp
+++ src/bin/scenarist-desktop/Application.cpp
@@ -39,7 +39,7 @@ Application::Application(int& _argc, char** _argv) :
 	setOrganizationName("DimkaNovikov labs.");
 	setOrganizationDomain("dimkanovikov.pro");
 	setApplicationName("Scenarist");
-	setApplicationVersion("0.6.0 beta 2");
+	setApplicationVersion("0.6.0 beta 3");
 
 	//
 	// Настроим стиль отображения внешнего вида приложения
