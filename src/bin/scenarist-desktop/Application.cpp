ba623e41f12fec2f9722982f134848530b0b210d
Dimka Novikov <to@dimkanovikov.pro>
2016-08-23 13:42:23 +0300

    Fix bug with closing current project on Mac OS X

--- src/bin/scenarist-desktop/Application.cpp
+++ src/bin/scenarist-desktop/Application.cpp
@@ -39,7 +39,7 @@ Application::Application(int& _argc, char** _argv) :
 	setOrganizationName("DimkaNovikov labs.");
 	setOrganizationDomain("dimkanovikov.pro");
 	setApplicationName("Scenarist");
-    setApplicationVersion("0.6.1 beta 3");
+    setApplicationVersion("0.6.1 beta 4");
 
 	//
 	// Настроим стиль отображения внешнего вида приложения
--- src/bin/scenarist-desktop/UserInterfaceLayer/Research/ResearchView.cpp
+++ src/bin/scenarist-desktop/UserInterfaceLayer/Research/ResearchView.cpp
@@ -94,6 +94,15 @@ void ResearchView::setSynopsisSettings(QPageSize::PageSizeId _pageSize, const QM
 
 void ResearchView::setResearchModel(QAbstractItemModel* _model)
 {
+    //
+    // Отключаем соединения от старой модели
+    //
+    if (QAbstractItemModel* oldModel = m_ui->researchNavigator->model()) {
+        disconnect(m_ui->researchNavigator->selectionModel(), &QItemSelectionModel::selectionChanged,
+                this, &ResearchView::currentResearchChanged);
+        disconnect(oldModel, &QAbstractItemModel::rowsInserted, this, &ResearchView::researchItemAdded);
+    }
+
 	//
 	// Загружаем модель
 	//
