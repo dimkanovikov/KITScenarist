f7b47c5cb875b469980f76926095e417e19b1337
Armijo <hvoltage@inbox.ru>
2016-09-09 21:13:37 +0300

    The License has been inserted

--- src/bin/scenarist-core/3rd_party/Widgets/PasswordLineEdit/PasswordLineEdit.cpp
+++ src/bin/scenarist-core/3rd_party/Widgets/PasswordLineEdit/PasswordLineEdit.cpp
@@ -1,3 +1,20 @@
+/*
+* Copyright (C) 2014 Dimka Novikov, to@dimkanovikov.pro
+* Copyright (C) 2016 Alexey Polushkin, armijo38@yandex.ru
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* General Public License for more details.
+*
+* Full license: http://dimkanovikov.pro/license/GPLv3
+*/
+
 #include "PasswordLineEdit.h"
 
 #include <QToolButton>
