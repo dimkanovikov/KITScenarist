2c7f5c3b5d865e0bf2b13302e13ab797de9cdf05
Armijo <hvoltage@inbox.ru>
2016-12-01 14:20:58 +0300

    Copyright to a new file

--- src/libs/webloader/src/WebLoaderGlobal.h
+++ src/libs/webloader/src/WebLoaderGlobal.h
@@ -1,3 +1,20 @@
+/*
+* Copyright (C) 2015 Dimka Novikov, to@dimkanovikov.pro
+* Copyright (C) 2016 Alexey Polushkin, armijo38@yandex.ru
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* Full license: http://dimkanovikov.pro/license/LGPLv3
+*/
+
 #ifndef WEBLOADERGLOBAL_H
 #define WEBLOADERGLOBAL_H
 
