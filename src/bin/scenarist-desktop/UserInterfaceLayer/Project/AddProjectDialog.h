1682a07823e3bbf34bdf41d7546ea64c0cdfd497
Dimka Novikov <to@dimkanovikov.pro>
2017-01-04 22:18:08 +0300

    Set focus on text fields after show add project and share dialogs

--- src/bin/scenarist-desktop/UserInterfaceLayer/Project/AddProjectDialog.h
+++ src/bin/scenarist-desktop/UserInterfaceLayer/Project/AddProjectDialog.h
@@ -4,70 +4,76 @@
 #include <3rd_party/Widgets/QLightBoxWidget/qlightboxdialog.h>
 
 namespace Ui {
-	class AddProjectDialog;
+    class AddProjectDialog;
 }
 
 
 namespace UserInterface
 {
-	/**
-	 * @brief Класс диалога авторизации пользователя
-	 */
-	class AddProjectDialog : public QLightBoxDialog
-	{
-		Q_OBJECT
+    /**
+     * @brief Класс диалога авторизации пользователя
+     */
+    class AddProjectDialog : public QLightBoxDialog
+    {
+        Q_OBJECT
 
-	public:
-		explicit AddProjectDialog(QWidget* _parent = 0);
-		~AddProjectDialog();
+    public:
+        explicit AddProjectDialog(QWidget* _parent = 0);
+        ~AddProjectDialog();
 
-		/**
-		 * @brief Установить возможность добавления проектов из облака
-		 */
-		void setIsRemoteAvailable(bool _isAvailable, bool _isEnabled = false, bool _isSelected = false);
+        /**
+         * @brief Установить возможность добавления проектов из облака
+         */
+        void setIsRemoteAvailable(bool _isAvailable, bool _isEnabled = false, bool _isSelected = false);
 
-		/**
-		 * @brief Создаётся локальный файл (true) или в облаке (false)
-		 */
-		bool isLocal() const;
+        /**
+         * @brief Создаётся локальный файл (true) или в облаке (false)
+         */
+        bool isLocal() const;
 
-		/**
-		 * @brief Название создаваемого проекта
-		 */
-		QString projectName() const;
+        /**
+         * @brief Название создаваемого проекта
+         */
+        QString projectName() const;
 
-		/**
-		 * @brief Путь создаваемого файла
-		 */
-		QString projectFilePath() const;
+        /**
+         * @brief Путь создаваемого файла
+         */
+        QString projectFilePath() const;
 
-		/**
-		 * @brief Нужно ли импортировать проект
-		 */
-		bool isNeedImport() const;
+        /**
+         * @brief Нужно ли импортировать проект
+         */
+        bool isNeedImport() const;
 
-		/**
-		 * @brief Файл из которого нужно экспоритровать проект
-		 */
-		QString importFilePath() const;
+        /**
+         * @brief Файл из которого нужно экспоритровать проект
+         */
+        QString importFilePath() const;
 
-	private:
-		/**
-		 * @brief Настроить представление
-		 */
-		void initView();
+    protected:
+        /**
+         * @brief Переопределяем для установки фокуса
+         */
+        QWidget* focusedOnExec() const override;
 
-		/**
-		 * @brief Настроить соединения для формы
-		 */
-		void initConnections();
+    private:
+        /**
+         * @brief Настроить представление
+         */
+        void initView();
 
-	private:
-		/**
-		 * @brief Интерфейс
-		 */
-		Ui::AddProjectDialog* m_ui;
-	};
+        /**
+         * @brief Настроить соединения для формы
+         */
+        void initConnections();
+
+    private:
+        /**
+         * @brief Интерфейс
+         */
+        Ui::AddProjectDialog* m_ui;
+    };
 }
 
 #endif // ADDPROJECTDIALOG_H
