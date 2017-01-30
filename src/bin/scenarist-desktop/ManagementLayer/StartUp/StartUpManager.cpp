218808b1bda41bc47e090f709fdf4decf31701f5
Armijo <hvoltage@inbox.ru>
2017-01-30 15:20:38 +0300

    Implemented saving feedback email

--- src/bin/scenarist-desktop/ManagementLayer/StartUp/StartUpManager.cpp
+++ src/bin/scenarist-desktop/ManagementLayer/StartUp/StartUpManager.cpp
@@ -394,6 +394,18 @@ void StartUpManager::checkCrashReports()
     //
     if (hasUnhandledReports) {
         CrashReportDialog dialog(m_view);
+
+        //
+        // Возьмем email из хранилища (тот же, что и для авторизации)
+        //
+        const QString email =
+            StorageFacade::settingsStorage()->value(
+                "application/email",
+                SettingsStorage::ApplicationSettings);
+        if (!email.isEmpty()) {
+            dialog.setEmail(email);
+        }
+
         QString handledReportPath = unhandledReportPath;
         if (dialog.exec() == CrashReportDialog::Accepted) {
             dialog.showProgress();
@@ -411,6 +423,16 @@ void StartUpManager::checkCrashReports()
             // Помечаем отчёт, как отправленный
             //
             handledReportPath += "." + SENDED;
+
+            //
+            // Сохраняем email, если ранее не было никакого
+            //
+            if (email.isEmpty()) {
+                StorageFacade::settingsStorage()->setValue(
+                "application/email",
+                dialog.email(),
+                SettingsStorage::ApplicationSettings);
+            }
         }
         //
         // Помечаем отчёт, как проигнорированный
