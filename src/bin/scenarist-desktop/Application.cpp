7f029e553ee3a718a533972050f86b656a68ad33
Dimka Novikov <to@dimkanovikov.pro>
2017-02-26 13:23:08 +0300

    Release 0.7.1 beta 3

--- src/bin/scenarist-desktop/Application.cpp
+++ src/bin/scenarist-desktop/Application.cpp
@@ -12,177 +12,177 @@
 #include <QTranslator>
 
 namespace {
-	/**
-	 * @brief Подготовить путь к файлу для сохранения
-	 */
-	static QString preparePath(const QString& _path) {
-		QString newPath = _path;
+    /**
+     * @brief Подготовить путь к файлу для сохранения
+     */
+    static QString preparePath(const QString& _path) {
+        QString newPath = _path;
 #ifdef Q_OS_MAC
-		//
-		// Это две разные буквы!
-		// Первую даёт нам мак, когда открываешь файл через двойной щелчок на нём
-		//
-		newPath.replace("й", "й");
+        //
+        // Это две разные буквы!
+        // Первую даёт нам мак, когда открываешь файл через двойной щелчок на нём
+        //
+        newPath.replace("й", "й");
 #endif
-		return newPath;
-	}
+        return newPath;
+    }
 }
 
 
 Application::Application(int& _argc, char** _argv) :
-	QApplication(_argc, _argv),
-	m_applicationManager(0)
+    QApplication(_argc, _argv),
+    m_applicationManager(0)
 {
-	//
-	// Настроим информацию о приложении
-	//
-	setOrganizationName("DimkaNovikov labs.");
-	setOrganizationDomain("dimkanovikov.pro");
-	setApplicationName("Scenarist");
-    setApplicationVersion("0.7.1 beta 2");
+    //
+    // Настроим информацию о приложении
+    //
+    setOrganizationName("DimkaNovikov labs.");
+    setOrganizationDomain("dimkanovikov.pro");
+    setApplicationName("Scenarist");
+    setApplicationVersion("0.7.1 beta 3");
 
-	//
-	// Настроим стиль отображения внешнего вида приложения
-	//
-	setStyle(QStyleFactory::create("Fusion"));
+    //
+    // Настроим стиль отображения внешнего вида приложения
+    //
+    setStyle(QStyleFactory::create("Fusion"));
 
-	//
-	// Загрузим шрифты в базу шрифтов программы, если их там ещё нет
-	//
-	QFontDatabase fontDatabase;
-	fontDatabase.addApplicationFont(":/Fonts/Courier New");
-	fontDatabase.addApplicationFont(":/Fonts/Courier Prime");
-	fontDatabase.addApplicationFont(":/Fonts/Courier Prime Bold");
-	fontDatabase.addApplicationFont(":/Fonts/Courier Prime Italic");
-	fontDatabase.addApplicationFont(":/Fonts/Courier Prime Bold Italic");
+    //
+    // Загрузим шрифты в базу шрифтов программы, если их там ещё нет
+    //
+    QFontDatabase fontDatabase;
+    fontDatabase.addApplicationFont(":/Fonts/Courier New");
+    fontDatabase.addApplicationFont(":/Fonts/Courier Prime");
+    fontDatabase.addApplicationFont(":/Fonts/Courier Prime Bold");
+    fontDatabase.addApplicationFont(":/Fonts/Courier Prime Italic");
+    fontDatabase.addApplicationFont(":/Fonts/Courier Prime Bold Italic");
 
-	//
-	// Настроим перевод приложения
-	//
-	initTranslation();
+    //
+    // Настроим перевод приложения
+    //
+    initTranslation();
 
-	//
-	// Настроим таймер определения простоя приложения
-	//
-	m_idleTimer.setInterval(3000);
-	connect(&m_idleTimer, &QTimer::timeout, [=] {
-		if (m_applicationManager != 0) {
-			postEvent(m_applicationManager, new QEvent(QEvent::User));
-		}
-	});
-	m_idleTimer.start();
+    //
+    // Настроим таймер определения простоя приложения
+    //
+    m_idleTimer.setInterval(3000);
+    connect(&m_idleTimer, &QTimer::timeout, [=] {
+        if (m_applicationManager != 0) {
+            postEvent(m_applicationManager, new QEvent(QEvent::User));
+        }
+    });
+    m_idleTimer.start();
 }
 
 void Application::setupManager(ManagementLayer::ApplicationManager *_manager)
 {
-	m_applicationManager = _manager;
+    m_applicationManager = _manager;
 }
 
 bool Application::notify(QObject* _object, QEvent* _event)
 {
-	//
-	// Работа с таймером определяющим простой приложения
-	//
-	if (_event->type() == QEvent::MouseMove
-		|| _event->type() == QEvent::MouseButtonPress
-		|| _event->type() == QEvent::MouseButtonDblClick
-		|| _event->type() == QEvent::KeyPress
-		|| _event->type() == QEvent::InputMethod
-		|| _event->type() == QEvent::Wheel
-		|| _event->type() == QEvent::Gesture) {
-		m_idleTimer.start();
-	}
+    //
+    // Работа с таймером определяющим простой приложения
+    //
+    if (_event->type() == QEvent::MouseMove
+        || _event->type() == QEvent::MouseButtonPress
+        || _event->type() == QEvent::MouseButtonDblClick
+        || _event->type() == QEvent::KeyPress
+        || _event->type() == QEvent::InputMethod
+        || _event->type() == QEvent::Wheel
+        || _event->type() == QEvent::Gesture) {
+        m_idleTimer.start();
+    }
 
-	return QApplication::notify(_object, _event);
+    return QApplication::notify(_object, _event);
 }
 
 bool Application::event(QEvent* _event)
 {
-	bool result = true;
-	if (_event->type() == QEvent::FileOpen
-		&& m_applicationManager != 0) {
-		QFileOpenEvent* fileOpenEvent = static_cast<QFileOpenEvent*>(_event);
-		m_applicationManager->openFile(::preparePath(fileOpenEvent->file()));
-	} else {
-		result = QApplication::event(_event);
-	}
+    bool result = true;
+    if (_event->type() == QEvent::FileOpen
+        && m_applicationManager != 0) {
+        QFileOpenEvent* fileOpenEvent = static_cast<QFileOpenEvent*>(_event);
+        m_applicationManager->openFile(::preparePath(fileOpenEvent->file()));
+    } else {
+        result = QApplication::event(_event);
+    }
 
-	return result;
+    return result;
 }
 
 void Application::initTranslation()
 {
-	//
-	// Определим язык перевода
-	//
-	const int language =
-			DataStorageLayer::StorageFacade::settingsStorage()->value(
-				"application/language",
-				DataStorageLayer::SettingsStorage::ApplicationSettings)
-			.toInt();
-	QString translationSuffix = QLocale::system().name();
-	translationSuffix.truncate(translationSuffix.lastIndexOf('_'));
-	QString qtTranslationSuffix = translationSuffix;
-	//
-	// ... если не удалось определить локаль, используем англоязычный перевод
-	//
-	QLocale::Language currentLanguage = QLocale::AnyLanguage;
-	if (translationSuffix.isEmpty()) {
-		translationSuffix = "en";
-		currentLanguage = QLocale::English;
-	}
+    //
+    // Определим язык перевода
+    //
+    const int language =
+            DataStorageLayer::StorageFacade::settingsStorage()->value(
+                "application/language",
+                DataStorageLayer::SettingsStorage::ApplicationSettings)
+            .toInt();
+    QString translationSuffix = QLocale::system().name();
+    translationSuffix.truncate(translationSuffix.lastIndexOf('_'));
+    QString qtTranslationSuffix = translationSuffix;
+    //
+    // ... если не удалось определить локаль, используем англоязычный перевод
+    //
+    QLocale::Language currentLanguage = QLocale::AnyLanguage;
+    if (translationSuffix.isEmpty()) {
+        translationSuffix = "en";
+        currentLanguage = QLocale::English;
+    }
 
-	if (language == 0) {
-		translationSuffix = "ru";
-		currentLanguage = QLocale::Russian;
-	} else if (language == 1) {
-		translationSuffix = "es";
-		currentLanguage = QLocale::Spanish;
-	} else if (language == 2) {
-		translationSuffix = "en";
-		currentLanguage = QLocale::English;
-	} else if (language == 3) {
-		translationSuffix = "fr";
-		currentLanguage = QLocale::French;
-	} else if (language == 4) {
-		translationSuffix = "kz";
-		qtTranslationSuffix = "ru";
-		currentLanguage = QLocale::Kazakh;
-	} else if (language == 5) {
-		translationSuffix = "ua";
-		qtTranslationSuffix = "ru";
-		currentLanguage = QLocale::Ukrainian;
-	} else if (language == 6) {
-		translationSuffix = "de";
-		qtTranslationSuffix = "de";
-		currentLanguage = QLocale::German;
-	}
+    if (language == 0) {
+        translationSuffix = "ru";
+        currentLanguage = QLocale::Russian;
+    } else if (language == 1) {
+        translationSuffix = "es";
+        currentLanguage = QLocale::Spanish;
+    } else if (language == 2) {
+        translationSuffix = "en";
+        currentLanguage = QLocale::English;
+    } else if (language == 3) {
+        translationSuffix = "fr";
+        currentLanguage = QLocale::French;
+    } else if (language == 4) {
+        translationSuffix = "kz";
+        qtTranslationSuffix = "ru";
+        currentLanguage = QLocale::Kazakh;
+    } else if (language == 5) {
+        translationSuffix = "ua";
+        qtTranslationSuffix = "ru";
+        currentLanguage = QLocale::Ukrainian;
+    } else if (language == 6) {
+        translationSuffix = "de";
+        qtTranslationSuffix = "de";
+        currentLanguage = QLocale::German;
+    }
 
-	QLocale::setDefault(QLocale(currentLanguage));
+    QLocale::setDefault(QLocale(currentLanguage));
 
-	//
-	// Для отличных от английского, подключаем переводы самой Qt
-	//
-	if (translationSuffix != "en") {
-		//
-		// Подключим файл переводов Qt
-		//
-		QTranslator* qtTranslator = new QTranslator;
-		qtTranslator->load(":/Translations/Translations/qt_" + qtTranslationSuffix + ".qm");
-		installTranslator(qtTranslator);
+    //
+    // Для отличных от английского, подключаем переводы самой Qt
+    //
+    if (translationSuffix != "en") {
+        //
+        // Подключим файл переводов Qt
+        //
+        QTranslator* qtTranslator = new QTranslator;
+        qtTranslator->load(":/Translations/Translations/qt_" + qtTranslationSuffix + ".qm");
+        installTranslator(qtTranslator);
 
-		//
-		// Подключим дополнительный файл переводов Qt
-		//
-		QTranslator* qtBaseTranslator = new QTranslator;
-		qtBaseTranslator->load(":/Translations/Translations/qtbase_" + qtTranslationSuffix + ".qm");
-		installTranslator(qtBaseTranslator);
-	}
+        //
+        // Подключим дополнительный файл переводов Qt
+        //
+        QTranslator* qtBaseTranslator = new QTranslator;
+        qtBaseTranslator->load(":/Translations/Translations/qtbase_" + qtTranslationSuffix + ".qm");
+        installTranslator(qtBaseTranslator);
+    }
 
-	//
-	// Подключим файл переводов программы
-	//
-	QTranslator* appTranslator = new QTranslator;
-	appTranslator->load(":/Translations/Translations/Scenarist_" + translationSuffix + ".qm");
-	installTranslator(appTranslator);
+    //
+    // Подключим файл переводов программы
+    //
+    QTranslator* appTranslator = new QTranslator;
+    appTranslator->load(":/Translations/Translations/Scenarist_" + translationSuffix + ".qm");
+    installTranslator(appTranslator);
 }
