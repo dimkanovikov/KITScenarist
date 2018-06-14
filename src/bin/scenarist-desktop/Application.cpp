c9e09c097a5a0add3aaadad85379e8531c3f82fd
dimkanovikov <to@dimkanovikov.pro>
2018-06-14 15:25:37 +0300

    Implemented drawing dialogues color marks #671

--- src/bin/scenarist-desktop/Application.cpp
+++ src/bin/scenarist-desktop/Application.cpp
@@ -42,7 +42,7 @@ Application::Application(int& _argc, char** _argv) :
     setOrganizationName("DimkaNovikov labs.");
     setOrganizationDomain("dimkanovikov.pro");
     setApplicationName("Scenarist");
-    setApplicationVersion("0.7.2 beta 7");
+    setApplicationVersion("0.7.2 beta 8");
 
     //
     // Настроим стиль отображения внешнего вида приложения
