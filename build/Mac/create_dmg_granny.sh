fbb763530f316d6e5b7c1320c0c894e48d46247c
dimkanovikov <to@dimkanovikov.pro>
2018-09-05 14:56:11 +0300

    Update mac installer related stuff

--- build/Mac/create_dmg_granny.sh
+++ build/Mac/create_dmg_granny.sh
@@ -14,40 +14,19 @@ cp ../../src/bin/scenarist-desktop/logo.icns ../Release/bin/scenarist-desktop/Sc
 #
 # скопируем app-файл в текущую папку
 #
-cp -R ../Release/bin/scenarist-desktop/Scenarist.app ./Scenarist.app
+cp -R ../Release/bin/scenarist-desktop/Scenarist.app ./Digipitch\ screenwriter.app
 
 #
 # подпишем app-файл
 #
-codesign --deep --force --verify --verbose --sign "Developer ID Application: Dmitry Novikov (9VJUEP4AA5)" Scenarist.app
-
-#
-# Создаём русский dmg-файл
-#
-./make_dmg.sh -i ../../src/bin/scenarist-desktop/logo.icns -b cover.png -c "462:252:176:258" -s "640:400"  Scenarist.app
-mv -f Scenarist.dmg scenarist-setup-$1.dmg
-codesign --force --sign "Developer ID Application: Dmitry Novikov (9VJUEP4AA5)" scenarist-setup-$1.dmg
+codesign --deep --force --verify --verbose --sign "Developer ID Application: Dmitry Novikov (9VJUEP4AA5)" Digipitch\ screenwriter.app
 
 #
 # Создаём английский dmg-файл
 #
-./make_dmg.sh -i ../../src/bin/scenarist-desktop/logo.icns -b cover_en.png -c "462:252:176:258" -s "640:400"  Scenarist.app
-mv -f Scenarist.dmg scenarist-setup-$1_en.dmg
-codesign --force --sign "Developer ID Application: Dmitry Novikov (9VJUEP4AA5)" scenarist-setup-$1_en.dmg
-
-#
-# Создаём испанский dmg-файл
-#
-./make_dmg.sh -i ../../src/bin/scenarist-desktop/logo.icns -b cover_es.png -c "462:252:176:258" -s "640:400"  Scenarist.app
-mv -f Scenarist.dmg scenarist-setup-$1_es.dmg
-codesign --force --sign "Developer ID Application: Dmitry Novikov (9VJUEP4AA5)" scenarist-setup-$1_es.dmg
-
-#
-# Создаём французский dmg-файл
-#
-./make_dmg.sh -i ../../src/bin/scenarist-desktop/logo.icns -b cover_fr.png -c "462:252:176:258" -s "640:400"  Scenarist.app
-mv -f Scenarist.dmg scenarist-setup-$1_fr.dmg
-codesign --force --sign "Developer ID Application: Dmitry Novikov (9VJUEP4AA5)" scenarist-setup-$1_fr.dmg
+./make_dmg.sh -i ../../src/bin/scenarist-desktop/logo.icns -b cover_en.png -c "462:252:176:258" -s "640:400"  Digipitch\ screenwriter.app
+mv -f Digipitch\ screenwriter.dmg digipitch-screenwriter-setup.dmg
+codesign --force --sign "Developer ID Application: Dmitry Novikov (9VJUEP4AA5)" digipitch-screenwriter-setup.dmg
 
 # удалить app-файл
-rm -R ./Scenarist.app
+rm -R ./Digipitch\ screenwriter.app
