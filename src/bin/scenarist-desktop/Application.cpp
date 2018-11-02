f9828ddb23dabed97dfddb30f7e0c45749bd2eb9
Dimka Novikov <to@dimkanovikov.pro>
2018-11-02 21:51:06 +0300

    Release 0.7.2 rc2a

--- src/bin/scenarist-desktop/Application.cpp
+++ src/bin/scenarist-desktop/Application.cpp
@@ -42,7 +42,7 @@ Application::Application(int& _argc, char** _argv) :
     setOrganizationName("DimkaNovikov labs.");
     setOrganizationDomain("dimkanovikov.pro");
     setApplicationName("Scenarist");
-    setApplicationVersion("0.7.2 rc 2");
+    setApplicationVersion("0.7.2 rc 2a");
 
     //
     // Настроим стиль отображения внешнего вида приложения
