fc61fac7d237b65a1f1658b4af74369542b567e8
dimkanovikov <to@dimkanovikov.pro>
2019-02-25 08:20:51 +0300

    Added Catalan spell checker dictionary #803

--- src/bin/scenarist-desktop/UserInterfaceLayer/Settings/SettingsView.cpp
+++ src/bin/scenarist-desktop/UserInterfaceLayer/Settings/SettingsView.cpp
@@ -768,6 +768,7 @@ void SettingsView::initData()
     m_ui->spellCheckingLanguage->addItem(tr("Armenian (Western)"), SpellChecker::ArmenianWestern);
     m_ui->spellCheckingLanguage->addItem(tr("Azerbaijani"), SpellChecker::Azerbaijani);
     m_ui->spellCheckingLanguage->addItem(tr("Belorussian"), SpellChecker::Belorussian);
+    m_ui->spellCheckingLanguage->addItem(tr("Catalan"), SpellChecker::Catalan);
     m_ui->spellCheckingLanguage->addItem(tr("English (GB)"), SpellChecker::EnglishGB);
     m_ui->spellCheckingLanguage->addItem(tr("English (US)"), SpellChecker::EnglishUS);
     m_ui->spellCheckingLanguage->addItem(tr("Farsi"), SpellChecker::Farsi);
