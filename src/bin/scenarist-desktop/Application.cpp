99f5b72a20d2eb5e6e5ea55800d7c14b02464ed8
dimkanovikov <to@dimkanovikov.pro>
2020-12-09 11:09:02 +0300

    Release 0.7.2 rc 10

--- src/bin/scenarist-desktop/Application.cpp
+++ src/bin/scenarist-desktop/Application.cpp
@@ -42,7 +42,7 @@ Application::Application(int& _argc, char** _argv) :
     setOrganizationName("DimkaNovikov labs.");
     setOrganizationDomain("dimkanovikov.pro");
     setApplicationName("Scenarist");
-    setApplicationVersion("0.7.2 rc 9k");
+    setApplicationVersion("0.7.2 rc 10");
 
     //
     // Настроим стиль отображения внешнего вида приложения
